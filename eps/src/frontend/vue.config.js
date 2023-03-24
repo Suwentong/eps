e428a407db8d1304bafa69411c7f11f1363c69b5
Suwentong <abdukadirov048@gmail.com>
2023-03-24 16:47:51 +0800

    Changed port to 8081 and enabled proxy on http://localhost:8080

--- eps/src/frontend/vue.config.js
+++ eps/src/frontend/vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
+module.exports = {
+  devServer: {
+    port: 8081,
+    proxy: 'http://localhost:8080'
+  },
   transpileDependencies: true
-})
+}
