deb3c4b086ed3dc203427afcc2abf3c8b194a4ac
Suwentong <abdukadirov048@gmail.com>
2023-06-30 15:19:11 +0800

    font-awesome.js

---
+++ eps/src/frontend/src/plugins/font-awesome.js
@@ -0,0 +1,27 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import {
+  faHome,
+  faUser,
+  faUserPlus,
+  faSignInAlt,
+  faSignOutAlt,
+  faPersonWalkingArrowRight,
+  faFileLines,
+  faUsers,
+  faArrowRightToCity,
+  faArrowUpRightFromSquare
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(faHome,
+            faUser,
+            faUserPlus,
+            faSignInAlt,
+            faPersonWalkingArrowRight,
+            faFileLines,
+            faUsers,
+            faArrowRightToCity,
+            faSignOutAlt,
+            faArrowUpRightFromSquare);
+
+export { FontAwesomeIcon };
